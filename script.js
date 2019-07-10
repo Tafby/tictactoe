f212ccc4fab3dd4fb78f79feac35fc5f665eb7e7
Tiffany <Tafby88@gmail.com>
2019-07-10 12:27:35 -0700

    added winning combos

---
+++ script.js
@@ -0,0 +1,13 @@
+let origboard;
+const humanPlayer = '0';
+const computerPlayer = 'X';
+const winningCombos = [
+	[ 0, 1, 2 ],
+	[ 3, 4, 5 ],
+	[ 6, 7, 8 ],
+	[ 0, 4, 8 ],
+	[ 0, 3, 6 ],
+	[ 1, 4, 7 ],
+	[ 2, 5, 8 ],
+	[ 6, 4, 2 ]
+];

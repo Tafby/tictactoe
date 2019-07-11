df177ad3e38c93039a2c7c5ffebf7fc7528cae9e
Tiffany <Tafby88@gmail.com>
2019-07-11 09:49:05 -0700

    added start game function

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ const winningCombos = [
 	[ 2, 5, 8 ],
 	[ 6, 4, 2 ]
 ];
+
+const cells = document.querySelectorAll('.cell');
+
+startGame();
+
+function startGame() {
+	document.querySelector('.endgame').style.display = 'none';
+	origboard = Array.from(Array(9).keys()); //create an array on 9 elements and get just the keys to a new array
+	for (let i = 0; i < cells.length; i++) {
+		cells[i].innerText = '';
+		// cells[i].removeProperty('background-color');
+		cells[i].addEventListener('click', turnClick, false);
+	}
+}
+function turnClick(square) {
+	turn(square.target.id, humanPlayer);
+}
